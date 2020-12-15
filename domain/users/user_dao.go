812363f300d5bc97550a106360e4c0e06df5302f
Antonio Bello <Antonio>
2020-12-15 09:23:30 +0400

    feat: changed to use GORM for creating and getting users

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -8,46 +8,24 @@ import (
 )
 
 const (
-  queryInsertUser ="INSERT INTO users(firstName, lastName, email, dateCreated) VALUES(?,?,?,?);"
+	queryInsertUser = "INSERT INTO users(firstName, lastName, email, dateCreated) VALUES(?,?,?,?);"
 )
 
 // Get user by id from db
 func (user *User) Get() *errors.RestError {
-  if err := database.Client.Ping(); err != nil {
-    panic(err)
-  }
-  result := user
-  if result == nil {
-    return errors.NewNotFoundError(fmt.Sprintf("User with id: %d not found", user.ID))
-  }
-  user.ID = result.ID
-  user.FirstName = result.FirstName
-  user.LastName = result.LastName
-  user.Email = result.Email
-  user.DateCreated = result.DateCreated
-  return nil
+	result := database.Client.First(user, user.ID)
+	if result.Error != nil {
+		return errors.NewNotFoundError(fmt.Sprintf("User with id: %d not found", user.ID))
+	}
+	return nil
 }
-
 
 //Save a user to db
 func (user *User) Save() *errors.RestError {
-  statement, err := database.Client.Prepare(queryInsertUser)
-  if err != nil {
-    return errors.NewInternalServerError(err.Error())
-  }
-  defer statement.Close()
-
-  insertResult, err := statement.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
-  if err != nil {
-    return errors.NewInternalServerError(
-      fmt.Sprintf("error when trying to save user: %s",err.Error()))
-  }
-  
-  userID, err := insertResult.LastInsertId()
-  if err != nil {
-    return errors.NewInternalServerError(
-      fmt.Sprintf("error when trying to save user: %s",err.Error()))
-  }
-  user.ID = userID
-  return nil
+	result := database.Client.Create(user)
+	if result.Error != nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", result.Error.Error()))
+	}
+	return nil
 }
--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -8,18 +8,18 @@ import (
 
 // User dto
 type User struct {
-  ID int64 `json:"id"`
-  FirstName string `json:"firstName"`
-  LastName string `json:"lastName"`
-  Email string `json:"email"`
-  DateCreated string `json:"dateCreated"`
+	ID          int64  `json:"id"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Email       string `json:"email"`
+	DateCreated string `json:"dateCreated"`
 }
 
 // Validate user struct
-func(user *User) Validate() *errors.RestError {
-  user.Email = strings.TrimSpace(strings.ToLower(user.Email))
-  if user.Email == "" {
-    return errors.NewBadRequestError("invalid email address")
-  }
-  return nil
+func (user *User) Validate() *errors.RestError {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
 }
